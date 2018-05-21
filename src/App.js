3a49fd3bcc0ec50cecf99d51de9363a60e3e52cd
Mayur Koli <kolimayurs@gmail.com>
2018-05-21 17:39:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      	<div className="img_design"><img src={gold_design} alt="Engagement" /><img src={og_img} alt="og_img" /></div>
+      	<div className="img_design"><img src={gold_design} alt="Engagement" /><img src={og_img} className="og_img" alt="og_img" /></div>
       	<div className="invite-text">YOU ARE CORDIALLY INVITED <br />TO CELEBRATE THE ENGAGEMENT OF</div>
       	<div className="names-box">
       	<div className="names"><span className="big_name">O</span>MKANT <br /> TO <br /><span className="big_name">A</span>RCHANA</div>
