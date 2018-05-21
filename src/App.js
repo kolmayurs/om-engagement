baa3ef885b3d1f72d9069ed5f292322f17c98c0d
Mayur Koli <kolimayurs@gmail.com>
2018-05-21 17:35:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import gold_design from './img/gold_design.png';
+import og_img from './img/eng.png';
 import Peacock from './Peacock'; 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      	<div className="img_design"><img src={gold_design} alt="Engagement" /></div>
+      	<div className="img_design"><img src={gold_design} alt="Engagement" /><img src={og_img} alt="og_img" /></div>
       	<div className="invite-text">YOU ARE CORDIALLY INVITED <br />TO CELEBRATE THE ENGAGEMENT OF</div>
       	<div className="names-box">
       	<div className="names"><span className="big_name">O</span>MKANT <br /> TO <br /><span className="big_name">A</span>RCHANA</div>
